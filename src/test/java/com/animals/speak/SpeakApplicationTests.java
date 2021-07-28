353f0b14b0580b29fd902b149e147130e7c4d36a
s fargali <sfargali@gmail.com>
2021-07-28 03:46:29 -0400

    added rest of directory

---
+++ src/test/java/com/animals/speak/SpeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animals.speak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
